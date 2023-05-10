06923826ff4cecd4811d48c869b0bbdc4fcd65d1
Boban Djordjevic <bdjordjevic990@gmail.com>
2023-05-10 12:23:30 +0200

    Create default Spring initializer application with Java17

---
+++ src/main/java/com/roninsolutions/sportbook/SportbookApplication.java
@@ -0,0 +1,13 @@
+package com.roninsolutions.sportbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roninsolutions/sportbook/SportbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roninsolutions.sportbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
